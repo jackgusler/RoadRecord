37e4c0e93c8b17a4334d26db534c86a4d6fb5b9e
jackgusler <guslerj1@newpaltz.edu>
2024-09-02 01:38:51 -0400

    colors working for auto profile pic

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 use Intervention\Image\ImageManager;
 
 class DatabaseSeeder extends Seeder
@@ -26,13 +25,36 @@ class DatabaseSeeder extends Seeder
 
         // Define a set of nature-inspired colors
         $natureColors = [
-            ['red' => 34, 'green' => 139, 'blue' => 34],   // ForestGreen
-            ['red' => 60, 'green' => 179, 'blue' => 113],  // MediumSeaGreen
-            ['red' => 32, 'green' => 178, 'blue' => 170],  // LightSeaGreen
-            ['red' => 107, 'green' => 142, 'blue' => 35],  // OliveDrab
-            ['red' => 46, 'green' => 139, 'blue' => 87],   // SeaGreen
-            ['red' => 0, 'green' => 128, 'blue' => 128],   // Teal
-            ['red' => 85, 'green' => 107, 'blue' => 47],   // DarkOliveGreen
+            'rgba(172, 162, 83, 0.5)',
+            'rgba(109, 128, 65, 0.5)',
+            'rgba(187, 186, 166, 0.5)',
+            'rgba(199, 170, 52, 0.5)',
+            'rgba(130, 149, 86, 0.5)',
+            'rgba(90, 96, 44, 0.5)',
+            'rgba(140, 145, 111, 0.5)',
+            'rgba(137, 165, 110, 0.5)',
+            'rgba(176, 177, 140, 0.5)',
+            'rgba(160, 145, 70, 0.5)',
+            'rgba(133, 147, 90, 0.5)',
+            'rgba(102, 160, 72, 0.5)',
+            'rgba(184, 207, 218, 0.5)',
+            'rgba(103, 139, 159, 0.5)',
+            'rgba(191, 219, 232, 0.5)',
+            'rgba(160, 194, 205, 0.5)',
+            'rgba(79, 96, 106, 0.5)',
+            'rgba(112, 139, 146, 0.5)',
+            'rgba(169, 173, 176, 0.5)',
+            'rgba(101, 125, 131, 0.5)',
+            'rgba(158, 135, 104, 0.5)',
+            'rgba(211, 185, 162, 0.5)',
+            'rgba(182, 147, 123, 0.5)',
+            'rgba(126, 103, 77, 0.5)',
+            'rgba(212, 212, 212, 0.5)',
+            'rgba(74, 78, 68, 0.5)',
+            'rgba(114, 103, 87, 0.5)',
+            'rgba(207, 191, 178, 0.5)',
+            'rgba(186, 139, 121, 0.5)',
+            'rgba(125, 118, 105, 0.5)'
         ];
 
         // Loop to create 10 users
@@ -43,8 +65,14 @@ class DatabaseSeeder extends Seeder
             // Randomly select a color from the set
             $selectedColor = $natureColors[array_rand($natureColors)];
 
-            // Apply the selected color to the image
-            $image->colorize($selectedColor['red'], $selectedColor['green'], $selectedColor['blue']);
+            $centerX = 256;
+            $centerY = 256;
+            $radius = 208;
+
+            $image->drawCircle($centerX, $centerY, function ($circle) use ($selectedColor, $radius) {
+                $circle->radius($radius); // radius of circle in pixels
+                $circle->background($selectedColor, 50); // background color
+            });
 
             // Convert the image to a blob
             $imageData = (string) $image->toPng();
